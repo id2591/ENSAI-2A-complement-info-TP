9911258800fb1556c056c351f81804455168c8f9
jmalet <julie.malet@eleve.ensai.fr>
2025-09-19 10:02:51 +0000

    attack dao

--- src/dao/attack_dao.py
+++ src/dao/attack_dao.py
@@ -42,7 +42,81 @@ class AttackDao(metaclass=Singleton):
             created = True
 
         return created
+    
 
+    def find_attack_by_id(self, id: int) -> AbstractAttack:
+        """
+        Get all attack type names and return a list
+
+        :return: An attack
+        :rtype: An AbstractAttack
+        """
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    SELECT
+                        a.id_attack,
+                        a.power,
+                        a.accuracy,
+                        a.element,
+                        a.attack_name,
+                        a.attack_description,
+                        at.attack_type_name,
+                    FROM tp.attack a
+                    JOIN tp.attack_type at
+                        ON a.id_attack_type = at.id_attack_type
+                    WHERE a.id_attack = %(id_attack)s
+                    """,
+                    {
+                        "id_attack": id,
+                    },
+                )
+                row = cursor.fetchone()
+        
+        if row is None:
+            return None
+        else:
+            return AttackFactory().instantiate_attack(
+                type=row["attack_type_name"],
+                id=row["id_attack"],
+                power=row["power"],
+                name=row["attack_name"],
+                description=row["attack_description"],
+                accuracy=row["accuracy"],
+                element=row["element"]
+            )
+
+
+    def find_all_attacks(self) -> List[str]:
+        """
+        Get all attack type names and return a list
+
+        :return: A list of all attacks
+        :rtype: List of str
+        """
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    
+                )
+
+                # to store raw results
+                res = cursor.fetchall()
+
+        # Create an empty list to store formatted results
+        attack: List[str] = []
+
+        # if the SQL query returned results (ie. res not None)
+        if res:
+            for row in res:
+                type_attack.append(row["attack_name"])
+
+                print(row["id_attack"])
+                print(row["attack_name"])
+                print(row["attack_description"])
+
+        return attack
 
 if __name__ == "__main__":
     # Pour charger les variables d'environnement contenues dans le fichier .env
